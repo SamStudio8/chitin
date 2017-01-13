6ecc6fc99c3c78bcdc0aea2bdf688497f651040d
Sam Nicholls <sam.n@studio8media.co.uk>
2017-01-13 10:44:31 +0000

    Monospace UUIDs on web

--- chitin/web/web.py
+++ chitin/web/web.py
@@ -3,14 +3,10 @@ from flask import render_template
 from chitin import record
 
 @record.app.route('/')
-def index():
-    return render_template('index.html', events=record.Event.query.order_by('timestamp DESC').limit(10))
-
-@record.app.route('/experiments')
 def experiment_list():
     return render_template('experiments.html', experiments=record.Experiment.query.all())
 
-@record.app.route('/runs/<experiment>')
+@record.app.route('/experiment/<experiment>')
 def experiment_detail(experiment):
     exp = record.Experiment.query.get_or_404(experiment)
     return render_template('experiment.html', exp=exp)
