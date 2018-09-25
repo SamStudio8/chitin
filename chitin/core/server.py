1aed17e5bfbe65f3d76e8569e703f97415bed58d
Sam Nicholls <sam.n@studio8media.co.uk>
2018-09-25 18:47:42 +0100

    bye chitin core

--- chitin/core/server.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-from chitin import conf
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + conf.DATABASE_PATH
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
